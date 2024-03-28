f6e4be0891ed0e39b534f5ca6302f093fd2686e9
Sarutapns <152773197+Sarutapns@users.noreply.github.com>
2024-03-29 02:08:30 +0700

    Update auth-route.js

--- back/routes/auth-route.js
+++ back/routes/auth-route.js
@@ -4,16 +4,16 @@ const authenticate = require('../middlewares/authenticate');
 const userController = require('../controllers/auth-controller');
 
 
-// สร้างเส้นทางสำหรับลงทะเบียนผู้ใช้ใหม่
+
 router.post('/register', userController.register);
 
-// สร้างเส้นทางสำหรับเข้าสู่ระบบ
+
 router.post('/login', userController.login);
 
-// สร้างเส้นทางสำหรับเรียกข้อมูลของผู้ใช้ปัจจุบัน
+
 router.get('/me', authenticate, userController.getme);
 
-// สร้างเส้นทางสำหรับเข้าสู่ระบบในฐานะผู้ดูแลระบบ
+
 router.post('/admin/login', userController.adminLogin);
 
 module.exports = router;
